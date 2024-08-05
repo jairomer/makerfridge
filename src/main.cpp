794fa2b4eb66d59983ef16f438db1c54c7cc77b8
synk <jairomer@protonmail.com>
2024-08-05 18:44:02 +0200

    Parametrize wifi settings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include <WiFi.h>
 
 void setup() {
     Serial.begin(115200);
+
+    WiFi.mode(WIFI_STA); // Set ESP32 t station mode
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
 }
 
 void loop() {
