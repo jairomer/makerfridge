b8e2e65869856c925b5adfb3b93bfa269b930dde
synk <jairomer@protonmail.com>
2024-08-05 21:10:43 +0200

    Basic machine state container.
    - Tests deliver a linking error.
    - We need to decouple the state machine from the Arduino framework.

--- lib/machine/machine.hpp
+++ lib/machine/machine.hpp
@@ -1,33 +1,34 @@
-#include "Arduino.h"
-#include <WiFi.h>
+#ifndef MACHINE_HPP
+#define MACHINE_HPP
+
+#include <string>
+#include <Arduino.h>
 
 const int TOTAL_PRODUCTS = 5;
 const int DIG_INPUT_RANGE = 1;
 const int DIG_OUTPUT_RANGE = DIG_INPUT_RANGE+DIG_INPUT_RANGE;
 
-typedef struct ProductPins
-{
+typedef struct ProductPins {
     int button;
     int actuator;
 } product_pins_t;
 
-typedef struct ProductStats
-{
+typedef struct ProductStats {
     int current_stock;
 } product_stats_t;
 
-typedef struct Product
-{
+typedef struct Product {
+    std::string name;
     int previous_button_state;
     product_pins_t pins;
     product_stats_t stats;
 } product_t;
 
-typedef struct Machine
-{
+typedef struct Machine {
     Machine() {
         // Initialize Machine State
         for (int i=0; i<TOTAL_PRODUCTS; i++) {
+            MachineProducts[i].name = "";
             MachineProducts[i].pins.button = DIG_INPUT_RANGE + i;
             MachineProducts[i].previous_button_state = LOW;
             MachineProducts[i].pins.actuator = DIG_OUTPUT_RANGE + i;
@@ -52,26 +53,4 @@ typedef struct Machine
     private: product_t MachineProducts[TOTAL_PRODUCTS];
 } machine_t;
 
-
-machine_t *machineState = new Machine();
-
-void setup() {
-    Serial.begin(115200);
-
-    // Connect ESP32 to the defined access point
-    WiFi.mode(WIFI_STA); 
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-    Serial.println("\nConnecting...");
-    while(WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(100);
-    }
-    Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
-    Serial.println(WiFi.localIP());
-}
-
-void loop() {
-    machineState->read_buttons();
-}
+#endif // MACHINE_HPP
---
+++ test/test_machine_state.cpp
@@ -0,0 +1,18 @@
+#include <unity.h>
+#include "machine.hpp"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void test_machine_state_initialization() {
+    TEST_ASSERT_TRUE(false);
+}
+
+int main(int argc, char **argv) {
+    RUN_TEST(test_machine_state_initialization);
+}
+
+
