4a2e014a58fb9916f0e0ef540648a576ef956ee9
synk <jairomer@protonmail.com>
2024-08-08 17:49:43 +0200

    feat: Achieved connection to WiFi and mDNS.
    Now you can ping <mdns>.local and receive
    a response.

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
+
 compiledb:
 	pio run -t compiledb
 
 test:
-	pio test
+	pio test --without-uploading -e native
+
+build:
+	pio run -e esp32dev 
 
 run:
-	pio run -e esp32dev
+	pio run -e esp32dev --target upload
+
+ota:
+	pio run -e esp32dev-ota --target upload
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ESPmDNS.h>
 
 #include "board_framework.hpp"
 #include "board_framework_arduino.hpp"
@@ -13,22 +14,39 @@ void setup() {
     machineState = new Machine(board);
 
     Serial.begin(115200);
+    Serial.println("Waiting 5s...");
+    delay(5000);
 
     // Connect ESP32 to the defined access point
     WiFi.mode(WIFI_STA); 
     WiFi.begin(WIFI_SSID, WIFI_PASS);
 
     Serial.println("\nConnecting...");
+    int i=0;
     while(WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
+        if (i == 100) {
+            i = 0;
+            Serial.println(WiFi.status());
+            Serial.println(WIFI_SSID);
+        }
+        i++;
         delay(100);
     }
     Serial.println("\nConnected to the WiFi network");
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
+
+    if (!MDNS.begin(MDNS_ADDR)) {
+        Serial.println("Error starting mDNS");
+        return;
+    }
+    Serial.println("mDNS responder started");
+
 }
 
 void loop() {
+    Serial.println("Reading buttons...");
     machineState->read_buttons();
-    delay(50);
+    delay(100);
 }
